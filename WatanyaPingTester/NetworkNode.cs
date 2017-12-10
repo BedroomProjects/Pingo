2729d31a4d77d2704bfa0df1ea52142a53182b40
Badr Ali <probadrali@gmail.com>
2017-12-10 11:55:16 +0200

    initial push

---
+++ WatanyaPingTester/NetworkNode.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net.NetworkInformation;
+
+namespace WatanyaPingTester
+{
+    class Antenna
+    {
+        private string ip;
+        private string name;
+        private bool reachable;
+        bool pending = true;
+
+        public Antenna(string n, string i)
+        {
+            this.ip = i;
+            this.name = n;
+            //sendPing();
+        }
+
+        public void ping()
+        {
+            Ping ping = new Ping();
+            PingReply rp;
+            rp = ping.Send(ip);
+            if (rp.Status == IPStatus.Success)
+            {
+                reachable = true;
+                pending = false;
+            }
+            else
+                if (rp.Status == IPStatus.TimedOut) pending = true;
+                else pending = false;
+            reachable = false;
+        }
+
+        public void sendPing()
+        {
+            Thread t = new Thread(ping);
+            t.Start();
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public string getIP()
+        {
+            return ip;
+        }
+
+        public string getStatus()
+        {
+            if (pending) return "Pending...";
+            if (reachable) return "Reachable";
+            else return "Not Reachable";
+        }
+    }
+}
+s
---
+++ WatanyaPingTester/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
+using System.Windows.Forms;
+
+namespace WatanyaPingTester
+{
+    static class Program
+    {
+        static List<Antenna> ants = new List<Antenna>();
+
+        //static void sendPing()
+        //{
+        //    string ip = "198.162.0.101";
+        //    Ping ping = new Ping();
+        //    PingReply rp;
+        //    rp = ping.Send(ip);
+        //    if (rp.Status == IPStatus.Success)
+        //        Console.WriteLine("Yos");
+        //    else
+        //        Console.WriteLine("Nop");
+        //    Console.WriteLine("Done.");
+        //}
+
+        static void displayAntennaStatus(Antenna a)
+        {
+            Console.WriteLine(a.getName() + "(" + a.getIP() + "): " + a.getStatus());
+        }
+
+        static void updateNetwork()
+        {
+            for (int i = 0; i < ants.Count; i++)
+            {
+                ants[i].sendPing();
+            }
+        }
+
+        static void updateDisplay()
+        {
+            for (int i = 0; i < ants.Count; i++)
+            {
+                displayAntennaStatus(ants[i]);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Antenna a = new Antenna("Sokhna 1", "10.0.10.101");
+            ants.Add(a);
+
+            a = new Antenna("Sokhna 2", "10.0.10.102");
+            ants.Add(a);
+
+            a = new Antenna("M1.1", "10.0.10.103");
+            ants.Add(a);
+
+            a = new Antenna("M1.2", "10.0.10.104");
+            ants.Add(a);
+
+            a = new Antenna("M1.3", "10.0.10.105");
+            ants.Add(a);
+
+            a = new Antenna("M1.4", "10.0.10.110");
+            ants.Add(a);
+
+            while (true)
+            {
+                updateNetwork();
+                updateDisplay();
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
