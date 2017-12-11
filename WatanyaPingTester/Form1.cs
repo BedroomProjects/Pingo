12f7da1ace928ab6de3cbeaf71a8ec92ff8339c0
TareqFahim <tareq.mfahim95@gmail.com>
2017-12-11 06:38:24 +0200

    Test Ui

--- WatanyaPingTester/Form1.cs
+++ WatanyaPingTester/Form1.cs
@@ -19,8 +19,25 @@ namespace WatanyaPingTester
 
         private void button1_Click(object sender, EventArgs e)
         {
-            NetworkNode m1 = new NetworkNode("M1", "Anttena", "Cairo - Sokhna", "192.168.1.1");
-            label1.Text = m1.getStatus();
+            List<NetworkNode> ants = new List<NetworkNode>();
+            NetworkNode a = new NetworkNode("Mokattam", "Anttena", "Cairo - Sokhna", "10.0.10.101");
+            ants.Add(a);
+            NetworkNode m1 = new NetworkNode("M1", "Anttena", "Cairo - Sokhna", "10.0.10.103");
+            ants.Add(m1);
+            m1 = new NetworkNode("M2", "Anttena", "Cairo - Sokhna", "10.0.10.111");
+            ants.Add(m1);
+            m1 = new NetworkNode("M3", "Anttena", "Cairo - Sokhna", "10.0.10.115");
+            ants.Add(m1);
+
+            label2.Text = ants.ElementAt(0).getStatus();
+            label4.Text = ants.ElementAt(1).getStatus();
+            label6.Text = ants.ElementAt(2).getStatus();
+            label8.Text = ants.ElementAt(3).getStatus();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
